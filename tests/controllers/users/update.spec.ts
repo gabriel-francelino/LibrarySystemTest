d1002e8aaa86d5bd6d6aad1506006eda7f20a923
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-03-06 17:18:23 -0300

    test: implementing the first two tests marked with 'todo'

--- tests/controllers/users/update.spec.ts
+++ tests/controllers/users/update.spec.ts
@@ -1,7 +1,7 @@
 import { UpdateUsersController } from '../../../src/controllers/users/update'
 import { logger } from '../../mocks/logger'
 import { usersRepositoryMock } from '../../mocks/users_repository'
-import { Book, NewBook } from '../../../src/controllers/models'
+import { User, NewUser } from '../../../src/controllers/models'
 import { fakerEN } from '@faker-js/faker'
 import { Request, Response } from 'express'
 
@@ -9,22 +9,19 @@ describe('UpdateUsersController', ()=> {
   function makeSut() {
     const controller = new UpdateUsersController(logger, usersRepositoryMock)
     
-    const newBookMock: NewBook = {
-      title: fakerEN.word.words(),
-      subtitle: fakerEN.word.words(),
-      publishing_company: fakerEN.company.name(),
-      published_at: fakerEN.date.anytime(),
-      authors: fakerEN.internet.userName(),
+    const newUserMock: NewUser = {
+      name: fakerEN.internet.userName(),
+      email: fakerEN.internet.email()
     }
 
-    const bookMock: Book = {
+    const userMock: User = {
       id: fakerEN.string.uuid(),
-      ...newBookMock
+      ...newUserMock
     }
 
     const requestMock = { 
-      body: newBookMock,
-      params: { id: bookMock.id } as any
+      body: newUserMock,
+      params: { id: userMock.id } as any
     } as Request
 
     const responseMock = {
@@ -39,7 +36,7 @@ describe('UpdateUsersController', ()=> {
     } as Response
 
     return {
-      controller, newBookMock, bookMock, requestMock, responseMock
+      controller, newUserMock, userMock, requestMock, responseMock
     }
   }
 
@@ -47,9 +44,31 @@ describe('UpdateUsersController', ()=> {
     jest.clearAllMocks()
   })
 
-  it.todo('should update and return user if the user was funded and if there is no other user with the same email')
+  it('should update and return user if the user was funded and if there is no other user with the same email', async () => {
+    const {controller, newUserMock, userMock, requestMock, responseMock} = makeSut()
+    jest.spyOn(usersRepositoryMock, 'getById').mockResolvedValueOnce(userMock)
+    jest.spyOn(usersRepositoryMock, 'getByEmail').mockResolvedValueOnce(undefined)
+    jest.spyOn(usersRepositoryMock, 'update').mockResolvedValueOnce(userMock)
 
-  it.todo('should return 404 statusCode and not update the user if there is no user with the id provided')
+    const promise = controller.update(requestMock, responseMock)
+
+    await expect(promise).resolves.not.toThrow()
+    expect(usersRepositoryMock.getById).toHaveBeenCalledWith(userMock.id)
+    expect(usersRepositoryMock.update).toHaveBeenCalledWith(userMock.id, newUserMock)
+    expect(responseMock.statusCode).toEqual(200)
+  })
+
+  it('should return 404 statusCode and not update the user if there is no user with the id provided', async () => {
+    const {controller, newUserMock, userMock, requestMock, responseMock} = makeSut()
+    jest.spyOn(usersRepositoryMock, 'getById').mockResolvedValueOnce(undefined)
+    jest.spyOn(usersRepositoryMock, 'update')
+
+    const promise = controller.update(requestMock, responseMock)
+
+    await expect(promise).resolves.not.toThrow()
+    expect(usersRepositoryMock.update).not.toHaveBeenCalled()
+    expect(responseMock.statusCode).toEqual(404)
+  })
 
   it.todo('should return 409 statusCode and not update the user if there is an user with the same email')
 
